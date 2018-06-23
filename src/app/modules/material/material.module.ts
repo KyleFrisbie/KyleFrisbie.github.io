7a336709037527a2cb7c29909236552da0a69803
Kyle L Frisbie <kyle.l.frisbie@gmail.com>
2018-06-22 22:03:48 -0600

    add material icon import

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 const MATERIAL_MODULES = [
     CommonModule,
     MatCardModule,
+    MatIconModule,
 ];
 
 @NgModule({
